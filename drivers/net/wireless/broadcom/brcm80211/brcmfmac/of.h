fddc875a545332c22ae0fdb72a3113f785c6fdb5
Phil Elwell <phil@raspberrypi.com>
2021-10-28 15:03:16 +0100

    brcmfmac: Read alternative firmware names from DT
    Add the ability to load the names of alternative firmwares from the
    Device Tree node. This permits separate firmwares for 43436s and 43438
    and allows downstream firmwares to coexist with upstream.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.h
@@ -5,9 +5,20 @@
 #ifdef CONFIG_OF
 void brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 		    struct brcmf_mp_device *settings);
+#ifdef CPTCFG_BRCMFMAC_SDIO
+struct brcmf_firmware_mapping *
+brcmf_of_fwnames(struct device *dev, u32 *map_count);
+#endif
 #else
 static void brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 			   struct brcmf_mp_device *settings)
 {
 }
+#ifdef CPTCFG_BRCMFMAC_SDIO
+static struct brcmf_firmware_mapping *
+brcmf_of_fwnames(struct device *dev, u32 *map_count)
+{
+	return NULL;
+}
+#endif
 #endif /* CONFIG_OF */
